178d7809cdac6af1068d9434229b722482de5117
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2024-02-06 20:58:41 -0300

    Add action listener

--- src/game/Game.java
+++ src/game/Game.java
@@ -33,6 +33,13 @@ public class Game extends JFrame {
         for (int i = 0; i < buttons.length ; i++) {
             buttons[i] = new JButton();
             panel.add(buttons[i]);
+            final int index = i;
+            buttons[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    buttonClicked(index);
+                }
+            });
         }
         // Adiciona o painel à janela principal
         getContentPane().add(panel, BorderLayout.CENTER);
@@ -47,13 +54,48 @@ public class Game extends JFrame {
         if(!gameEnded && board[index / 3][index % 3] == '\0') {
             buttons[index].setText(String.valueOf(currentPlayer));
             board[index/3][index%3] = currentPlayer;
+            if (checkWinner(currentPlayer)) {
+                JOptionPane.showMessageDialog(this, "Jogador " + currentPlayer + " venceu!");
+                gameEnded = true;
+            } else if (isBoardFull()) {
+                JOptionPane.showMessageDialog(this, "Empate!");
+                gameEnded = true;
+            } else {
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
         }
     }
     private boolean checkWinner(char player) {
-
+        // Verificar linhas
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+                return true;
+            }
+        }
+        // Verificar colunas
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == player && board[1][j] == player && board[2][j] == player) {
+                return true;
+            }
+        }
+        // Verificar diagonais
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+            return true;
+        }
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+            return true;
+        }
+        return false;
     }
     private boolean isBoardFull() {
-
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '\0') {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
     public static void main(String[] args) {
 
